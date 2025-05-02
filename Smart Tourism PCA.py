b6fb9eb6bd105e6a73ef9da1b23625a6900aa961
coco1207 <1094880899@qq.com>
2025-05-02 22:09:05 +0800

    Update Smart Tourism PCA.py

--- Smart Tourism PCA.py
+++ Smart Tourism PCA.py
@@ -7,7 +7,7 @@ file_path = ("/Users/cocoweng/Desktop/database/Step 1 Processing Data/Tourism/Da
 df_traditional = pd.read_excel(file_path)
 
 pca_traditional_results = []
-n_components_list = {}  # 记录每年主成分数量
+n_components_list = {}  
 
 years = df_traditional["Year"].unique()
 
@@ -16,30 +16,24 @@ for year in years:
     provinces = df_year["Province"].values
     df_year_features = df_year.drop(columns=["Province", "Year"])
 
-    # 标准化
+    
     scaler = StandardScaler()
     scaled_data = scaler.fit_transform(df_year_features)
-
-    # 确定主成分个数（累计方差贡献率85%）
+    
     pca_full = PCA()
     pca_full.fit(scaled_data)
 
-    # 计算累计方差贡献率
     cumulative_variance = np.cumsum(pca_full.explained_variance_ratio_)
 
-    # 选择累计方差贡献率 >= 85% 的最小主成分个数
     n_components = np.argmax(cumulative_variance >= 0.85) + 1
-    n_components_list[year] = n_components  # 记录主成分个数
+    n_components_list[year] = n_components 
 
-    # 使用选定的主成分数进行PCA
     pca = PCA(n_components=n_components)
     principal_components = pca.fit_transform(scaled_data)
 
-    # 计算加权综合指数
     explained_variance = pca.explained_variance_ratio_
     index_scores = np.sum(principal_components * explained_variance, axis=1)
 
-    # 存储结果
     temp_df = pd.DataFrame({
         "Province": provinces,
         "Year": year,
